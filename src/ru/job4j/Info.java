32589d8096cbb64333783e2358313b6df55fa23a
Eugen <Alexandrov>
2020-06-28 20:43:15 +0300

    3.4. Заголовок в файле java [#314607]

---
+++ src/ru/job4j/Info.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Info {
+    private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("d.MM.YYYY");
+    public static void main(String [] args) {
+        System.out.println(dtf.format(LocalDateTime.now()));
+    }
+}
