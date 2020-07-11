cd22e1c385f1ffd5e556a39d0d9b8721544a84df
Eugen <Alexandrov>
2020-07-11 15:38:19 +0300

    1.7. Состояние объекта [#314732]

---
+++ src/ru/job4j/oop/Battery.java
@@ -0,0 +1,22 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int value;
+
+    public Battery(int size) {
+        this.value = size;
+    }
+
+    public void exchange(Battery another) {
+        this.value += another.value;
+        another.value = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery b1 = new Battery(10);
+        Battery b2 = new Battery(5);
+        System.out.println("first battery: " + b1.value + ". second battery: " + b2.value);
+        b1.exchange(b2);
+        System.out.println("first battery: " + b1.value + ". second battery: " + b2.value);
+    }
+}
