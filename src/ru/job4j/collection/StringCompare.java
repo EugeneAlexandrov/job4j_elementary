a844c0459dd72395036ca95869528e56f31c950d
Eugen <Alexandrov>
2020-07-31 23:40:52 +0300

    3. Компаратор для строк [#314664] fix

--- src/ru/job4j/collection/StringCompare.java
+++ src/ru/job4j/collection/StringCompare.java
@@ -5,21 +5,12 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        int lenght = Math.max(o1.length(), o2.length());
-        char[][] word = new char[2][lenght];
-        for (int i = 0; i < o1.length(); i++) {
-            word[0][i] = o1.charAt(i);
-        }
-        for (int i = 0; i < o2.length(); i++) {
-            word[1][i] = o2.charAt(i);
-        }
+        int lenght = Math.min(o1.length(), o2.length());
         for (int i = 0; i < lenght; i++) {
-            if (word[0][i] < word[1][i]) {
-                return -1;
-            } else if (word[0][i] > word[1][i]) {
-                return 1;
+            if (o1.charAt(i) != o2.charAt(i)) {
+                return o1.charAt(i) - o2.charAt(i);
             }
         }
-        return 0;
+        return o1.length() - o2.length();
     }
 }
