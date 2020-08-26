10fcb4c1ab5bfa3a6656092ab6cdbee647f73acd
Eugen <Alexandrov>
2020-08-26 13:35:43 +0300

    2. Local-Variable Type Inference [#314531]

--- src/ru/job4j/search/PhoneDictionary.java
+++ src/ru/job4j/search/PhoneDictionary.java
@@ -1,10 +1,11 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Predicate;
 
 public class PhoneDictionary {
-    private ArrayList<Person> persons = new ArrayList<>();
+    private List<Person> persons = new ArrayList<>();
 
     public void add(Person person) {
         this.persons.add(person);
@@ -16,14 +17,13 @@ public class PhoneDictionary {
      * @param key Ключ поиска.
      * @return Список подощедщих пользователей.
      */
-    public ArrayList<Person> find(String key) {
+    public List<Person> find(String key) {
         Predicate<Person> combinePhone = x -> x.getPhone().contains(key);
         Predicate<Person> combineName = x -> x.getName().contains(key);
         Predicate<Person> combineSurname = x -> x.getSurname().contains(key);
         Predicate<Person> combineAddress = x -> x.getAddress().contains(key);
-
-        ArrayList<Person> result = new ArrayList<>();
-        for (Person person : persons) {
+        List<Person> result = new ArrayList<>();
+        for (var person : persons) {
             if (combinePhone.or(combineName).or(combineSurname).or(combineAddress).test(person)) {
                 result.add(person);
             }
